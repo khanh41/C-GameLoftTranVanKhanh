4e854f07ddddf5c0613eb06eee819e1a23c2ccc9
Your Name <you@example.com>
2019-11-20 15:03:10 +0700

    TicTacToeV2

--- TichTacToe/TichTacToe/main.cpp
+++ TichTacToe/TichTacToe/main.cpp
@@ -1,76 +1,128 @@
 #include<iostream>
 #include <time.h>
 using namespace std;
-bool check(int a[3][3]) {
-	for (int i = 0; i < 2; i++) {
-		if (a[0][0] == i && a[1][1] == i && a[2][2] == i) return true;
-		if (a[0][2] == i && a[1][1] == i && a[2][0] == i) return true;
-		for (int j = 0; j < 3; j++) {
-			if (a[j][0] == i && a[j][1] == i && a[j][2] == i) return true;
-			if (a[0][j] == i && a[1][j] == i && a[2][j] == i) return true;
+
+#define size 10 // do dai va rong cua ban co
+#define tickX 1
+#define tickO 0
+#define lengthWin 4 // do dai chien thang
+#define fill size*size-1 // het o trong de danh
+int init(int& select, string& playerO, string& playerX, int board[size][size]);
+bool check(int board[size][size], int tick, int row, int col);
+void print(int a[size][size]);
+bool change(string player, int board[size][size], int& row, int& col, int tick);
+int main() {
+	srand(time(NULL));
+	int select, row, col;
+	bool player1Win = rand() % 2;
+	int board[size][size];
+	string playerO, playerX;
+
+	//loi gioi thieu, nhap cac tham so
+	init(select, playerO, playerX, board);
+
+	//play
+	for (int i = 0; i < size * size; i++) {
+		string index;
+		player1Win = !player1Win;
+		if (player1Win) {
+			if (change(playerO, board, row, col, tickO)) break;
+		}
+		else if (change(playerX, board, row, col, tickX)) break;
+		print(board);
+		if (i == fill) cout << "Draw" << endl;
+	}
+	print(board);
+	return 0;
+}
+
+int init(int& select, string& playerO, string& playerX, int board[size][size]) {
+	cout << "Welcome To Tic-tac-toe game! Play with your way!" << endl
+		<< "If you find any problem, please contact john.nguyen@gameloft.com" << endl;
+	cout << "SELECT YOUR MODE (1 - PLAY GAME, OTHERS - EXIT GAME): ";
+	cin >> select;
+	if (select != 1) return 0;
+	cout << "Enter Player 1-O Name: ";
+	cin >> playerO;
+	cout << "Enter Player 2-X Name: ";
+	cin >> playerX;
+	print(board);
+	return 1;
+}
+bool check(int board[size][size], int tick, int row, int col) {
+	//check duong cheo
+	//tick = tickX || tick = tickO
+	int dem = 0;
+	int sovonglap = row + lengthWin + 1; //row - lengthWin + 1 + lengthWin * 2;
+	for (int i = row - lengthWin + 1, j = col + lengthWin - 1; i < sovonglap; i++, j--) {
+		if (i < 0 || j < 0) continue;
+		if (board[i][j] == tick) dem++;
+		else dem = 0;
+		if (dem == lengthWin) {
+			if (board[i + 1][j - 1] != 1 - tick || board[i - lengthWin][j + lengthWin] != 1 - tick)
+				return true;
+		}
+	}
+	dem = 0;
+	for (int i = row - lengthWin + 1, j = col - lengthWin + 1; i < sovonglap; i++, j++) {
+		if (i < 0 || j < 0) continue;
+		if (board[i][j] == tick) dem++;
+		else dem = 0;
+		if (dem == lengthWin) {
+			if (board[i + 1][j + 1] != 1 - tick || board[i - lengthWin][col - lengthWin] != 1 - tick)
+				return true;
+		}
+	}
+	//check duong thang theo cot
+	dem = 0;
+	for (int i = row - lengthWin + 1; i < sovonglap; i++) {
+		if (i < 0) continue;
+		if (board[i][col] == tick) dem++;
+		else dem = 0;
+		if (dem == lengthWin) {
+			if (board[i + 1][col] == 1 - tick || board[i - lengthWin][col] == 1 - tick)
+				return true;
+		}
+	}
+	//check duong thang theo hang
+	dem = 0;
+	sovonglap = col + lengthWin + 1;
+	for (int j = col - lengthWin + 1; j < sovonglap; j++) {
+		if (j < 0) j = 0;
+		if (board[row][j] == tick) dem++;
+		else dem = 0;
+		if (dem == lengthWin) {
+			if (board[row][j + 1] == 1 - tick || board[row][j - lengthWin] == 1 - tick)
+				return true;
 		}
 	}
 	return false;
 }
-void print(int a[3][3]) {
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (a[i][j] == 1) cout << "X";
-			else if (a[i][j] == 0) cout << "O";
-			else cout << "-";
+void print(int a[size][size]) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (a[i][j] == 1) cout << " X ";
+			else if (a[i][j] == 0) cout << " O ";
+			else cout << " - ";
 		}
 		cout << endl;
 	}
 }
-int main() {
-	cout << "Welcome To Tic-tac-toe game! Play with your way!" << endl
-		<< "If you find any problem, please contact john.nguyen@gameloft.com" << endl;
-	cout << "SELECT YOUR MODE (1 - PLAY GAME, OTHERS - EXIT GAME): ";
-	int select,row,col;
-	srand(time(NULL));
-	bool player1Win= rand() % 2;
-	int arr[3][3];
-	string player1, player2;
-	cin >> select;
-	if (select != 1) return 0;
-	cout << "Enter Player 1 Name: ";
-	cin >> player1;
-	cout << "Enter Player 2 Name: ";
-	cin >> player2;
-	for (int i = 0; i < 9; i++) {
-		string index;
-		player1Win = !player1Win;
-		if (player1Win) {
-			do {
-				cout << "Your Turn "<<player1<<" - O: ";
-				cin >> index;
-				row = (int)index[0] - '0';
-				col = (int)index[1] - '0';
-			} while (arr[row][col] == 0 || arr[row][col] == 1 || row > 2 || row < 0 || col>2 || col < 0);
-			arr[(int)index[0]-'0'][(int)index[1] - '0'] = 0;
-			if(i>2)
-				if (check(arr)) {
-					cout << player1<<" Win"<<endl;
-					break;
-				}
-		}
-		else {
-			do {
-				cout << "Your Turn "<<player2<<" - X: ";
-				cin >> index;
-				row = (int)index[0] - '0';
-				col = (int)index[1] - '0';
-			} while (arr[row][col] == 0 || arr[row][col] == 1 || row>2 || row <0 || col>2 || col<0);
-			arr[row][col] = 1;
-			if(i>2)
-				if (check(arr)) {
-					cout << player2<<" Win"<<endl;
-					break;
-				}
-		}
-		print(arr);
-		if (i == 8)cout << "Draw"<<endl;
+bool change(string player, int board[size][size], int& row, int& col, int tick) {
+	string index;
+	do {
+		if (tick == 0) cout << "Your Turn " << player << " - O: ";
+		else cout << "Your Turn " << player << " - X: ";
+		cin >> index;
+		row = (int)index[0] - '0';
+		col = (int)index[1] - '0';
+	} while (board[row][col] == 0 || board[row][col] == 1 || row > size - 1 || row < 0
+		|| col>size - 1 || col < 0);
+
+	board[(int)index[0] - '0'][(int)index[1] - '0'] = tick;
+	if (check(board, tick, row, col)) {
+		cout << player << " Win" << endl;
+		return true;
 	}
-	print(arr);
-	return 0;
+	return false;
 }
