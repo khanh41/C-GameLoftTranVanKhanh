695196fcd6fb371d16516ecfad568c519bbc1ec5
Your Name <you@example.com>
2019-11-22 16:56:55 +0700

    Doing About Patient

--- Object/WriteVirus/WriteVirus/Patient.cpp
+++ Object/WriteVirus/WriteVirus/Patient.cpp
@@ -1,1 +1,28 @@
 #include "Patient.h"
+
+Patient::Patient()
+{
+	m_resistance = 20; // random
+	m_state = 1; // 1 - ALIVE
+	DoStart();
+}
+
+Patient::Patient(int m_resistance, list<Virus*> m_virusList, int m_state)
+{
+	this->m_resistance = m_resistance;
+	this->m_virusList = m_virusList;
+	this->m_state = m_state;
+}
+
+Patient::~Patient()
+{
+//	delete this->m_virusList;
+}
+
+void Patient::DoStart()
+{
+	srand(time(NULL));
+	int VirusSize = rand() % 11 + 10;
+
+	m_state = 1;
+}
--- Object/WriteVirus/WriteVirus/Patient.h
+++ Object/WriteVirus/WriteVirus/Patient.h
@@ -1,6 +1,19 @@
 #pragma once
+#include<iostream>
+#include "Virus.h"
+#include <list>
+#include<time.h>
+using namespace std;
 class Patient
 {
-
+private:
+	int m_resistance; // his natural resistance
+	list<Virus*> m_virusList; // The current virus in his body
+	int m_state; // 0 – DIE, 1 - ALIVE
+public:
+	Patient();
+	Patient(int, list<Virus*>, int);
+	~Patient();
+	void DoStart();
 };
 
