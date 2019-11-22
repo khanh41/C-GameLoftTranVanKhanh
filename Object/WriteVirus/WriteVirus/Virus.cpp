b4817f09e300fbe4129d53c74eb5f9f7998296b8
Your Name <you@example.com>
2019-11-22 16:35:52 +0700

    Done About Virus

--- Object/WriteVirus/WriteVirus/Virus.cpp
+++ Object/WriteVirus/WriteVirus/Virus.cpp
@@ -49,3 +49,104 @@ bool Virus::ReduceResistance(int medicine_resistance)
 	}
 	return false;
 }
+
+void FluVirus::DoBorn()
+{
+	LoadADNInformation();
+	srand(time(NULL));
+	this->m_color = rand() % 2 ? 0x0000ff : 0xff0000;
+}
+
+FluVirus* FluVirus::DoClone()
+{
+	FluVirus *flu = new FluVirus();
+	flu->m_color = this->m_color;
+	flu->m_dna = this->m_dna;
+	flu->m_resistance = this->m_resistance;
+	return flu;
+}
+
+void FluVirus::DoDie()
+{
+	delete[] m_dna;
+	m_color = NULL;
+	m_resistance = NULL;
+}
+
+void FluVirus::InitResistance()
+{
+	srand(time(NULL));
+	this->m_resistance = rand() % 11 + 10;
+}
+
+void FluVirus::SetColor(int m_color)
+{
+	this->m_color = m_color;
+}
+
+int FluVirus::GetColor()
+{
+	return this->m_color;
+}
+
+DengueVirus::DengueVirus()
+{
+	this->m_dna = '\0';
+	this->m_protein[0] = '\0';
+	this->m_resistance = 10;
+}
+
+DengueVirus::DengueVirus(char* m_dna, int m_resistance, char m_protein[])
+{
+	this->m_dna = m_dna;
+	SetProtein(m_protein);
+	this->m_resistance = m_resistance;
+}
+
+void DengueVirus::DoBorn()
+{
+	LoadADNInformation();
+	srand(time(NULL));
+	int random = rand() % 3;
+	if (random == 0) SetProtein("NS3");
+	if (random == 1) SetProtein("NS5");
+	if (random == 2) SetProtein("E");
+}
+
+DengueVirus* DengueVirus::DoClone()
+{
+	DengueVirus* deng = new DengueVirus();
+	deng->SetProtein(this->m_protein);
+	deng->m_dna = this->m_dna;
+	deng->m_resistance = this->m_resistance;
+	return deng;
+}
+
+void DengueVirus::DoDie()
+{
+	delete[] m_dna;
+	delete[] m_protein;
+	m_resistance = NULL;
+}
+
+void DengueVirus::InitResistance()
+{
+	srand(time(NULL));
+	if(this->m_protein=="NS3") this->m_resistance = rand() % 10 + 1;
+	if(this->m_protein=="NS5") this->m_resistance = rand() % 10 + 11;
+	if(this->m_protein=="E")   this->m_resistance = rand() % 10 + 21;
+}
+
+void DengueVirus::SetProtein(const char m_protein[])
+{
+	int i = 0;
+	while(m_protein != '\0'){
+		this->m_protein[i] = m_protein[i];
+		i++;
+	}
+}
+
+char* DengueVirus::GetProtein()
+{
+	return this->m_protein;
+}
--- Object/WriteVirus/WriteVirus/Virus.h
+++ Object/WriteVirus/WriteVirus/Virus.h
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<string>
 #include<fstream>
+#include<time.h>
 using namespace std;
 class Virus
 {
@@ -15,9 +16,34 @@ public:
 	Virus(const Virus&);
 	void LoadADNInformation(); // load m_dna from file 
 	bool ReduceResistance(int); // when call TakeMedicine: m_resistance -= medicine_resistance, <=0 destroyed virus
-	virtual void DoBorn();
-	virtual void DoClone();
-	virtual void DoBDie();
-	virtual void InitResistance();
+	virtual void DoBorn() = 0;
+	virtual Virus* DoClone() = 0;
+	virtual void DoDie() = 0;
+	virtual void InitResistance() = 0;
 };
 
+class FluVirus:public Virus {
+private:
+	int m_color;//blue (value 0x0000ff) or red (value 0xff0000)
+public:
+	void DoBorn();
+	FluVirus *DoClone();
+	void DoDie();
+	void InitResistance();
+	void SetColor(int);
+	int GetColor();
+};
+
+class DengueVirus :public Virus {
+private:
+	char m_protein[4];
+public:
+	DengueVirus();
+	DengueVirus(char*,int,char[]);
+	void DoBorn();
+	DengueVirus* DoClone();
+	void DoDie();
+	void InitResistance();
+	void SetProtein(const char[]);
+	char* GetProtein();
+};
